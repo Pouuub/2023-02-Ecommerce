9df8bbcbff0f1e8acac4f7ce29d25ee4e8bbab4e
Spacantho <devbox1001@gmail.com>
2023-02-03 10:39:22 +0100

    login front

--- assets/js/registrationErrors.js
+++ assets/js/registrationErrors.js
@@ -6,17 +6,23 @@ form.addEventListener("submit", function(e){
     xhr.open("POST", "/register");
     xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
     xhr.onreadystatechange = function() {
+        console.log(this.status);
         if (this.readyState === XMLHttpRequest.DONE && this.status === 422) {
             var errors = JSON.parse(xhr.responseText).errors;
             
             var ul = document.getElementById('errors');
             ul.classList.add('bg-red-500');
+            ul.classList.add('p-4');
             for (key in errors) {      
                 var li = document.createElement("li");
                 li.appendChild(document.createTextNode(errors[key][0]));
                 ul.appendChild(li);
             }
-
+        }
+            
+        if (this.status === 200) {
+            document.cookie = "flavor=choco; SameSite=Strict; Secure;";
+            window.location.replace("/test");
         }
     }
     xhr.send(formData)
